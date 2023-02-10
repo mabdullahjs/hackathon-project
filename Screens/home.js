aac6ef267b62ab9445a1d24c555c2079bc77d172
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-02-10 22:15:57 +0500

    camera done

--- Screens/home.js
+++ Screens/home.js
@@ -1,12 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+// 
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  PermissionsAndroid,
+} from 'react-native';
+import React from 'react';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 const Home = () => {
-  return (
-    <View>
-      <Text style={{color:"black"}}>Home</Text>
-    </View>
-  )
-}
+  let options = {savePhotos: true, mediaType: 'photo'};
 
-export default Home
+  const openCamera = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      const picture = await launchCamera(options);
+      console.log('picture', picture);
+    } else {
+      console.log('Error Succed');
+    }
+  };
+
+  const openGallery = async () => {
+    const result = await launchImageLibrary(options);
+    console.log(result);
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <TouchableOpacity
+        style={{padding: 10, backgroundColor: '#000', marginVertical: 20}}>
+        <Text style={{color: 'white', fontSize: 15}} onPress={openCamera}>
+          Open Camera
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{padding: 10, backgroundColor: '#000', marginVertical: 20}}>
+        <Text style={{color: 'white', fontSize: 15}} onPress={openGallery}>
+          Open Gallery
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Home;
